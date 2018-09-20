221c010895ff7199fb95aaa32561d2c0ff366e5f
liliang <liliang1222@gmail.com>
2018-09-20 18:17:49 +0800

    使用fs.stat()获得文件相关信息

--- readStreamDemo/server.js
+++ readStreamDemo/server.js
@@ -9,18 +9,26 @@ var server = http.createServer(function(req, res){
 	var url = parse(req.url);
 	var path = join(root, url.pathname);
 
-	var stream = fs.createReadStream(path);
+	fs.stat(path, function(err, stat){
+		if(err){
+			if('ENOENT' == err.code){ //文件不存在
+				res.statusCode = 404;
+				res.end('Not Found');
+			}else{
+				res.statusCode = 500;
+				res.end('Internal Server Error');
+			}
+		}else{
+			res.setHeader('Content-Length', stat.size);
+			
+			var stream = fs.createReadStream(path);
 
-	// stream.on('data', function(chunk){
-	// 	res.write(chunk);
-	// });
-	// stream.on('end', function(){
-	// 	res.end();
-	// });
-	stream.pipe(res);
-	stream.on('error', function(err){
-		res.statusCode = 500;
-		res.end('Internal Server Error');
-	});
+			stream.pipe(res);
+			stream.on('error', function(err){
+				res.statusCode = 500;
+				res.end('Internal Server Error');
+			});
+		}
+	})
 });
 server.listen(3000);
