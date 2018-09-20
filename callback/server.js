ef8c82180537125b303b93266d27f85c3cc6ed7c
liliang <liliang1222@gmail.com>
2018-09-20 14:30:34 +0800

    回调处理

---
+++ callback/server.js
@@ -0,0 +1,26 @@
+var http = require('http')
+var fs = require('fs');
+
+http.createServer(function(req, res){
+	if(req.url == '/'){
+		fs.readFile('./titles.json', function(err, data){
+			if(err){
+				console.error(err);
+				res.end('Server Error');
+			}else{
+				var titles = JSON.parse(data.toString());
+				fs.readFile('./template.html', function(err, data){
+					if(err){
+						console.error(err);
+						res.end('Server Eror');
+					}else{
+						var tmp1 =  data.toString();
+						var html = tmp1.replace('%', titles.join('</li><li>'));
+						res.writeHead(200, {'Content-Type':'text/html'});
+						res.end(html);
+					}
+				});
+			}
+		});
+	}
+}).listen(8000, "127.0.0.1");
