91d9f65729bcd90da1fa79e68881363d5870bdba
liliang <liliang1222@gmail.com>
2018-09-20 14:40:50 +0800

    创建中间函数减少嵌套

--- callback/server.js
+++ callback/server.js
@@ -3,24 +3,37 @@ var fs = require('fs');
 
 http.createServer(function(req, res){
 	if(req.url == '/'){
-		fs.readFile('./titles.json', function(err, data){
-			if(err){
-				console.error(err);
-				res.end('Server Error');
-			}else{
-				var titles = JSON.parse(data.toString());
-				fs.readFile('./template.html', function(err, data){
-					if(err){
-						console.error(err);
-						res.end('Server Eror');
-					}else{
-						var tmp1 =  data.toString();
-						var html = tmp1.replace('%', titles.join('</li><li>'));
-						res.writeHead(200, {'Content-Type':'text/html'});
-						res.end(html);
-					}
-				});
-			}
-		});
+		getTitles(res);
 	}
 }).listen(8000, "127.0.0.1");
+
+function getTitles(res){
+	fs.readFile('./titles.json', function(err, data){
+			if(err){
+				hadError(err, res);
+			}else{
+				getTemplate(JSON.parse(data.toString()), res);
+			}
+		});
+}
+
+function getTemplate(titles, res){
+	fs.readFile('./template.html', function(err, data){
+		if(err){
+			hadError(err, res);
+		}else{
+			formatHtml(titles, data.toString(), res);
+		}
+	});
+}
+
+function formatHtml(titles, tmp1, res){
+	var html = tmp1.replace('%', titles.join('</li><li>'));
+	res.writeHead(200, {'Content-Type':'text/html'});
+	res.end(html);
+}
+
+function hadError(err, res){
+	console.error(err);
+	res.end('Server Error');
+}
